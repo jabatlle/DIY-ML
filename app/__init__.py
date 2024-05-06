22892e0e81e4a2be20690a691f700a11cb6ec219
jabatlle <114434629+jabatlle@users.noreply.github.com>
2024-05-05 20:16:00 -0400

    queue implementation

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask
 from flask_pymongo import PyMongo
 from flask_uploads import UploadSet, configure_uploads, DOCUMENTS
+from .celery import make_celery
 
 app = Flask(__name__)
 
@@ -18,5 +19,13 @@ ALLOWED_EXTENSIONS = {'pdf'}
 uploads = UploadSet('documents', DOCUMENTS)
 app.config['UPLOADS_DEFAULT_DEST'] = UPLOAD_FOLDER  # Set the upload destination
 configure_uploads(app, uploads)
+
+# Celery configuration
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+app.config['CELERY_BROKER_URL'] = CELERY_BROKER_URL
+app.config['CELERY_RESULT_BACKEND'] = CELERY_RESULT_BACKEND
+
+celery = make_celery(app)
 
 from app import routes
